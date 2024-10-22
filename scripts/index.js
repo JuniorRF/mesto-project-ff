b4d3e558544aeac4feffbb910649fe56ce7f4a7f
unknown <demchakromka@gmail.com>
2024-10-23 00:48:21 +0300

    start +

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,32 @@
 // @todo: Темплейт карточки
-const cardTemplate = document.querySelector('#card-template');
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
-// console.log(initialCards);
 const placesList = document.querySelector('.places__list');
-// console.log(placesList);
-
+// const deleteButton = document.querySelector('.card__delete-button')
 
 // @todo: Функция создания карточки
 function createCard(name, link) {
-  const cardTemplate = document.querySelector('#card-template');
-  const cardElement = cardTemplate.cloneNode(true);
-  console.log(cardElement)
-  console.log(cardElement.querySelector('h2'))
-  // console.log(cardElement.querySelector('.card__image'))
-  // cardElement.querySelector('.card__title').textContent = name;
-  // cardElement.querySelector('.card__image').src = link;
-  // placesList.append(cardElement);
+  const cardElement = cardTemplate.querySelector('li').cloneNode(true);
+  cardElement.querySelector('.card__title').textContent = name;
+  cardElement.querySelector('.card__image').src = link;
+  cardElement.querySelector('.card__image').style.display = 'block'
+
+  // выберем кнопку удаления
+  const deleteButton = cardTemplate.querySelector('.card__delete-button');
+
+  // добавим обработчик
+  deleteButton.addEventListener('click', function () {
+    const listItem = deleteButton.closest('.card');
+    listItem.remove();
+  });
+
+  // cardElement.querySelector('.card__delete-button').addEventListener('click', function (evt) {
+  //   console.log(evt.target)
+  //   deleteCard(evt)
+  // });
+
+  placesList.append(cardElement);
 }
 
 initialCards.forEach((item) => {
@@ -24,5 +34,10 @@ initialCards.forEach((item) => {
 })
 
 // @todo: Функция удаления карточки
+// function deleteCard(card) {
+//   card.target.remove()
+// }
+
+// добавим обработчик
 
 // @todo: Вывести карточки на страницу
