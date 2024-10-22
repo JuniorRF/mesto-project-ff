9cff5199ca44aa9cf82b082b22b6841eb18007c4
junior <demchakromka@gmail.com>
2024-10-22 20:54:37 +0300

    start

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,27 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template');
 
 // @todo: DOM узлы
+// console.log(initialCards);
+const placesList = document.querySelector('.places__list');
+// console.log(placesList);
+
 
 // @todo: Функция создания карточки
+function createCard(name, link) {
+  const cardTemplate = document.querySelector('#card-template');
+  const cardElement = cardTemplate.cloneNode(true);
+  console.log(cardElement)
+  console.log(cardElement.querySelector('h2'))
+  // console.log(cardElement.querySelector('.card__image'))
+  // cardElement.querySelector('.card__title').textContent = name;
+  // cardElement.querySelector('.card__image').src = link;
+  // placesList.append(cardElement);
+}
+
+initialCards.forEach((item) => {
+  createCard(item.name, item.link);
+})
 
 // @todo: Функция удаления карточки
 
