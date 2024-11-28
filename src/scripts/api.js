818ef5011eb99c81203337e98d7e62a6450299f4
unknown <demchakromka@gmail.com>
2024-11-28 23:37:15 +0300

    начало 7 ПР

---
+++ src/scripts/api.js
@@ -0,0 +1,7 @@
+const config = {
+    baseUrl: 'https://nomoreparties.co/v1/cohort-28',
+    headers: {
+      authorization: '683f0ca7-4c94-44bb-b57a-83b864273a26',
+      'Content-Type': 'application/json'
+    }
+  }
---
+++ src/scripts/validation.js
@@ -0,0 +1,7 @@
+function enableValidation() {
+
+}
+
+function clearValidation() {
+    
+}
